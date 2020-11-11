b22f2a503dcf625b82d6d8f59d42d2bd40da9fdc
TheWriteApp <thewrittingapp@gmail.com>
2020-11-11 23:27:45 +0100

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { BadgePageComponent } from './badge-page/badge-page.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, BadgePageComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/badge-page/badge-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-badge-page",
+  templateUrl: "./badge-page.component.html",
+  styleUrls: ["./badge-page.component.css"]
+})
+export class BadgePageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
