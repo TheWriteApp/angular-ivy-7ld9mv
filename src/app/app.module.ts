1d9c82878393d2b872da429d8c4e136c92f05458
TheWriteApp <thewrittingapp@gmail.com>
2020-11-15 10:54:14 +0100

    1 file modified, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { BadgePageComponent } from './badge-page/badge-page.component';
-import { BadgeComponent } from './badge/badge.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { BadgePageComponent } from "./badge-page/badge-page.component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, BadgePageComponent, BadgeComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, BadgePageComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
